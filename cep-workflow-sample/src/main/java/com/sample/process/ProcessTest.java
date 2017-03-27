acaa9c3c40a6764dbcbd742671a9a0f67744eb08
epictrave <flacks12@gmail.com>
2017-03-27 23:52:02 +0900

    add sample code

---
+++ cep-workflow-sample/src/main/java/com/sample/process/ProcessTest.java
@@ -0,0 +1,26 @@
+package com.sample.process;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample file to launch a process.
+ */
+public class ProcessTest {
+
+    public static void startProcess() {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-process");
+
+            // start a new process instance
+            kSession.startProcess("cep-process");
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+}
